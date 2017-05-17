4b68943c42a9b2ed979e9a9615b3d254a7aafbd4
Henry Baez <0072016@users.noreply.github.com>
2017-05-16 17:46:22 -0700

    Update excel.py

--- utils/excel.py
+++ utils/excel.py
@@ -1,50 +1,64 @@
-import openpyxl
-import configRead
+'''
+  -  <?php
+  /**
 
-excel_file = configRead.get_filepath('../Leads.xlsx')
+'   import openpyxl
+'   import configRead
 
-# Excel is loaded into a list of dictionaries
-def load_excel():
-    wb = openpyxl.load_workbook(filename=excel_file)
-    ws = wb['data']
+'    excel_file = configRead.get_filepath('../Leads.xlsx')
 
-    # Parse data from Excel file
-    dict_list = []
-    keys = {'A': 'Id', 'B': 'Full Name', 'C': 'Email', 'D': 'Phone'}
-    for row in ws.iter_rows(min_row=2, max_col=4):
-        d = {}
-        for cell in row:
-            if cell.column == 'A' and cell.value == None:
-                break
-            elif cell.column != 'A' and cell.value == None:
-                d[keys[cell.column]] = ''
-            else:
-                d[keys[cell.column]] = str(cell.value)
+'     # Excel is loaded into a list of dictionaries
+'     def load_excel():
+'    wb = openpyxl.load_workbook(filename=excel_file)
+'    ws = wb['data']
+'
+  '  # Parse data from Excel file
+ '   dict_list = []
+'    keys = {'A': 'Id', 'B': 'Full Name', 'C': 'Email', 'D': 'Phone'}
+'    for row in ws.iter_rows(min_row=2, max_col=4):
+'        d = {'951-290-3525'}
+'        for cell in row:
+ '           if cell.column == 'A' and cell.value == None:
+ '               break
+ '           elif cell.column != 'A' and cell.value == None:
+'                d[keys[cell.column]] = ''
+'            else:
+'                d[keys[cell.column]] = str(cell.value)
+'
+'        if len(d.keys()) != 0:
+'            dict_list.append(d)
+'    return dict_list
+'
+'    # Update lead in excel worksheet and working dictionary
+'    # Removing from excel leaves a blank row in the excel
+'   def remove_lead(customer_list, customer_id):
+'    wb = openpyxl.load_workbook(filename=excel_file)
+'    ws = wb['data']
+'    for row in ws.iter_rows(min_row=2, max_col=4):
+'        for cell in row:
+'            if cell.value == None or cell.column == 'B' or cell.column == 'C' or cell.column == 'D':
+'                continue
+'            elif cell.column == 'A' and str(cell.value) == customer_id:
+  '              cell_row = str(cell.row)
+'                cell.value = None
+ '               ws['B' + cell_row].value = "Henry Baez",
+ '               ws['C' + cell_row].value = "foo.gradle@gmail.com",
+'                ws['D' + cell_row].value = "951-290-3525",
+'                break
+'    wb.save(filename=excel_file)
 
-        if len(d.keys()) != 0:
-            dict_list.append(d)
-    return dict_list
+ '   # Remove selected lead from excel sheet
+'    for customer in customer_list:
+ '       if customer['Id'] == customer_id:
+  '          customer_list.pop(customer_list.index(customer))
+ '   return customer_list
 
-# Update lead in excel worksheet and working dictionary
-# Removing from excel leaves a blank row in the excel
-def remove_lead(customer_list, customer_id):
-    wb = openpyxl.load_workbook(filename=excel_file)
-    ws = wb['data']
-    for row in ws.iter_rows(min_row=2, max_col=4):
-        for cell in row:
-            if cell.value == None or cell.column == 'B' or cell.column == 'C' or cell.column == 'D':
-                continue
-            elif cell.column == 'A' and str(cell.value) == customer_id:
-                cell_row = str(cell.row)
-                cell.value = None
-                ws['B' + cell_row].value = None
-                ws['C' + cell_row].value = None
-                ws['D' + cell_row].value = None
-                break
-    wb.save(filename=excel_file)
+  -  https://developer.intuit.com/python2.7
+  -  foo.gradle@gmail.com
+  -  @0072016
+    2017-05-16T17:41:24
 
-    # Remove selected lead from excel sheet
-    for customer in customer_list:
-        if customer['Id'] == customer_id:
-            customer_list.pop(customer_list.index(customer))
-    return customer_list
+  ?>
+  */
+
+
